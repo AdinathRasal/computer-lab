037707161164dc638dca841493741926ffe6fc5e
AdinathRasal <119794464+AdinathRasal@users.noreply.github.com>
2025-07-19 18:38:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-docs = {}  # room -> code
+docs = {}
 
 @app.route('/', defaults={'room': 'lab'})
 @app.route('/<room>')
@@ -12,17 +12,28 @@ def index(room):
     return render_template('index.html', room=room)
 
 @socketio.on('join')
-def on_join(data):
+def handle_join(data):
     room = data['room']
     join_room(room)
     emit('update', docs.get(room, ""), room=request.sid)
 
 @socketio.on('edit')
-def on_edit(data):
-    room = data['room']
-    text = data['text']
-    docs[room] = text
-    emit('update', text, room=room, include_self=False)
+def handle_edit(data):
+    docs[data['room']] = data['text']
+    emit('update', data['text'], room=data['room'], include_self=False)
+
+# WebRTC signaling
+@socketio.on('webrtc-offer')
+def webrtc_offer(data):
+    emit('webrtc-offer', data, room=data['room'], include_self=False)
+
+@socketio.on('webrtc-answer')
+def webrtc_answer(data):
+    emit('webrtc-answer', data, room=data['room'], include_self=False)
+
+@socketio.on('webrtc-candidate')
+def webrtc_candidate(data):
+    emit('webrtc-candidate', data, room=data['room'], include_self=False)
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000)
